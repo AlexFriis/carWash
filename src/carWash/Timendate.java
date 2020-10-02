a066ef0b7a65a26a9c6123be009c14ec3a97e93f
AlexFriis <alex3567@stud.kea.dk>
2020-10-02 14:53:03 +0200

    time&date

---
+++ src/carWash/Timendate.java
@@ -0,0 +1,16 @@
+package src.carWash;
+
+import java.util.Date;
+//import java.util.SimpleDateFormat;
+
+public class Timendate {
+    public static void main(String[] args) {
+        localTime();
+    }
+
+    private static void localTime() {
+        Date date = new java.util.Date();
+        System.out.println(date);
+    }
+
+}
